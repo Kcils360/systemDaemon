bc2698900125d5efcc99ef095c175d30d3b19082
Gregory <Dukes.GregoryD@gmail.com>
2025-01-27 11:53:32 -0800

    Update CheckTime.cs

--- systemDaemon/lib/CheckTime.cs
+++ systemDaemon/lib/CheckTime.cs
@@ -6,16 +6,16 @@ namespace systemDaemon
     public class CheckTime
     {
         ///<summary>
-        /// This will run a dateTime check. If it is 11:00 PM (2300 hrs) it will run the NightlySync() function.
+        /// This will run a dateTime check. If it is 11:00 PM (2300 hrs) it will run the main executable function.
         ///</summary>
-        ///<param name="config">The IConfigurationRoot object that passes to the NightlySync() function</param>
+        ///<param name="config">The IConfigurationRoot object that passes to the main executable function</param>
         public void RunCheck(IConfigurationRoot config)
         {
             DateTime dt = DateTime.Now;
             Console.WriteLine(dt + " execute RunCheck");
             if (dt.TimeOfDay.Hours == 23) //23 = 11:00PM
             {
-                Logger.log("runLogger", "Initialize NightlySync()");
+                Logger.log("runLogger", "Initialize main executable function");
                 //new task to run here ex: Console.Writeline("11:00, RunCheck works");
             }
         }
